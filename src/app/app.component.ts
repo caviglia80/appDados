c85079c05e00470ef3b05a26b1b13226248982ac
Francisco <caviglia80@gmail.com>
2024-03-02 21:48:09 +0100

    .r

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appDados';
+  dadoI = '../assets/img/dice1.png';
+  dadoD = '../assets/img/dice4.png';
+  numero1: number = 1;
+  numero2: number = 4;
+
+  tirarDados() {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+    this.dadoI = `../assets/img/dice${this.numero1}.png`;
+    this.dadoD = `../assets/img/dice${this.numero2}.png`;
+  }
+
+
+
 }
